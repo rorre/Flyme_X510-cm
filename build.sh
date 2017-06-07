bffea0c3f3214ba044c769ce6bde25ebde2edf84
Ubuntu <ubuntu@box146.localdomain>
2017-06-07 00:08:24 +0000

    Switch branch and dont upload target files

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ mkdir ~/bin
 PATH=~/bin:$PATH
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
-yes | repo init -u https://github.com/Rendyindo/manifest -b mm-test
+yes | repo init -u https://github.com/Rendyindo/manifest -b marshmallow-6.0
 repo sync -c
 source build/envsetup.sh
 cX510
 flyme fullota
 sudo apt install wput
-wput out/*.zip ftp://$USR:$PAWD@uploads.androidfilehost.com:21/
+wput out/f*.zip ftp://$USR:$PAWD@uploads.androidfilehost.com:21/
