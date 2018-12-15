4203d72124ab94fc1d67b2d3248608a649e3b95d
Rendy Arya Kemal <rendyarya22@gmail.com>
2018-12-15 12:04:11 +0700

    Add TeleService fix and build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+cd ~/
+mkdir a
+cd a
+mkdir ~/bin
+PATH=~/bin:$PATH
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+yes | repo init -u https://github.com/Rendyindo/manifest -b nougat-7.1
+repo sync -c
+source build/envsetup.sh
+cd devices
+rm -rf X510
+git clone https://github.com/rorre/Flyme_X510-cm X510 -b los-rework
+cd X510
+flyme fullota
+sudo apt install wput
+export DATEA=$(date +"%d%m%y")
+wput out/f*.zip ftp://$USR:$PAWD@uploads.androidfilehost.com:21/
